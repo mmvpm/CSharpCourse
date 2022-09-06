2c05cf6295b18c44c84679bcdfa386b61fbca5a7
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-06 23:48:36 +0300

    add MinStack

---
+++ hw01/MinStack/MinStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MinStack
+{
+    public class MinStack<T> where T : IComparable
+    {
+        private Stack<T> Stack { get;  }
+        private Stack<T> PrefixMins { get; }
+
+        public MinStack()
+        {
+            Stack = new Stack<T>();
+            PrefixMins = new Stack<T>();
+        }
+
+        public void Push(T element)
+        {
+            Stack.Push(element);
+            if (PrefixMins.Count == 0 || element.CompareTo(PrefixMins.Peek()) < 0)
+            {
+                PrefixMins.Push(element);
+            }
+            else
+            {
+                PrefixMins.Push(PrefixMins.Peek());
+            }
+        }
+
+        public T Pop()
+        {
+            PrefixMins.Pop();
+            return Stack.Pop();
+        }
+
+        public T Peek()
+        {
+            return Stack.Peek();
+        }
+
+        public T MinValue()
+        {
+            return PrefixMins.Peek();
+        }
+
+        public override string ToString()
+        {
+            return "Stack(" + string.Join(", ", Stack) + ")";
+        }
+    }
+}
---
+++ hw01/MinStack/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MinStack
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var minStack = new MinStack<int>();
+            
+            minStack.Push(5);
+            minStack.Push(2);
+            minStack.Push(4);
+            minStack.Push(1);
+            minStack.Push(3);
+            Console.WriteLine(minStack);            // Stack(3, 1, 4, 2, 5)
+            Console.WriteLine(minStack.Peek());     // 3
+            Console.WriteLine(minStack.MinValue()); // 1
+            Console.WriteLine();
+
+            minStack.Pop();
+            minStack.Pop();
+            Console.WriteLine(minStack);            // Stack(4, 2, 5)
+            Console.WriteLine(minStack.Peek());     // 4
+            Console.WriteLine(minStack.MinValue()); // 2
+        }
+    }
+}
