1df0045666f9d2343b23cece082ef75d7636f711
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-06 23:30:10 +0300

    add HashTable

---
+++ hw01/HashTable/HashTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HashTable
+{
+    public class HashTable<TK, TV>
+    {
+        private List<List<KeyValuePair<TK, TV>>> Storage { get; }
+
+        public HashTable(int size = 16)
+        {
+            Storage = Enumerable.Repeat(0, size).Select(_ => new List<KeyValuePair<TK, TV>>()).ToList();
+        }
+
+        public void Add(TK key, TV value)
+        {
+            var index = GetKeyIndex(key);
+            Storage[index].Add(new KeyValuePair<TK, TV>(key, value));
+        }
+
+        public void Remove(TK key)
+        {
+            var index = GetKeyIndex(key);
+            Storage[index].RemoveAll(pair => pair.Key.Equals(key));
+        }
+        
+        public bool Contains(TK key)
+        {
+            return FindPair(key).Key != null;
+        }
+
+        public TV Find(TK key)
+        {
+            return FindPair(key).Value;
+        }
+
+        public override string ToString()
+        {
+            var pairs = Storage
+                .SelectMany(x => x)
+                .Select(pair => pair.Key + " -> " + pair.Value);
+            return "{" + string.Join(", ", pairs) + "}";
+        }
+
+        private int GetKeyIndex(TK key)
+        {
+            return key.GetHashCode() % Storage.Count;
+        }
+
+        private KeyValuePair<TK, TV> FindPair(TK key)
+        {
+            var index = GetKeyIndex(key);
+            var neededPair = Storage[index].Find(pair => pair.Key.Equals(key));
+            return neededPair;
+        }
+    }
+}
---
+++ hw01/HashTable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HashTable
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            var table = new HashTable<string, int>();
+
+            table.Add("a", 1);
+            table.Add("b", 2);
+            table.Add("d", 4);
+            Console.WriteLine(table); // {b -> 2, d -> 4, a -> 1}
+
+            table.Remove("b");
+            table.Remove("c");
+            Console.WriteLine(table); // {d -> 4, a -> 1}
+
+            Console.WriteLine();
+            Console.WriteLine(table.Contains("c")); // False
+            Console.WriteLine(table.Find("c"));     // 0
+            Console.WriteLine(table.Contains("d")); // True
+            Console.WriteLine(table.Find("d"));     // 4
+        }
+    }
+}
