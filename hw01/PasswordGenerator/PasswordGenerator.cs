ab0e52662c7c9b5c9fee76fddc4c5b2d67f63c5f
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-07 01:00:58 +0300

    add PasswordGenerator

---
+++ hw01/PasswordGenerator/PasswordGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace PasswordGenerator
+{
+    public class PasswordGenerator
+    {
+        private readonly Random _random;
+
+        private const string Underscore = "_";
+        private const string Digits = "0123456789";
+        private const string LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        private const string UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        
+        public PasswordGenerator()
+        {
+            _random = new Random();
+        }
+
+        public string Generate()
+        {
+            var countUnderscore = 1;
+            var passwordLength = _random.Next(6, 21);
+            var countUpperCase = _random.Next(2, passwordLength - countUnderscore + 1);
+            var countDigits = _random.Next(0, Math.Min(passwordLength - countUnderscore - countUpperCase, 5) + 1);
+            var countLowerCase = passwordLength - countUnderscore - countUpperCase - countDigits;
+
+            var digits = RandomChoice(Digits, countDigits);
+            var lowerCase = RandomChoice(LowerCaseLetters, countLowerCase);
+            var upperCase = RandomChoice(UpperCaseLetters, countUpperCase);
+
+            var password = Underscore + lowerCase + upperCase;
+            password = RandomChoice(password, password.Length);
+            for (var i = 0; i < digits.Length; ++i)
+            {
+                password = password.Insert(2 * i, digits[i].ToString());
+            }
+
+            return password;
+        }
+        
+        private string RandomChoice(string s, int count)
+        {
+            return string.Join("", s.OrderBy(_ => _random.Next()).Take(count));
+        }
+    }
+}
---
+++ hw01/PasswordGenerator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PasswordGenerator
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var passwordGenerator = new PasswordGenerator();
+
+            for (var i = 0; i < 10; ++i)
+            {
+                Console.WriteLine(passwordGenerator.Generate());
+            }
+            /*
+                SEeZMfjz_RagQIskw
+                2FKaCHxrUlWSuE_GP
+                4V8_2Z1S3T
+                O_EDMIKT
+                3R6V8FGJ_MKYATNcPUID
+                ERX_LK
+                3STZBKIA_CDLPFVONMH
+                6k9y2Od_cfWbIrGAUJ
+                7B9Z_HGODRLvTUdJMFQS
+                0_jORslZ
+             */
+        }
+    }
+}
