688284279c14f942530852fee966df4dc257c1dc
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-13 15:09:57 +0300

    add immutable type

--- hw02/ImmutableType/ImmutablePoint.cs
+++ hw02/ImmutableType/ImmutablePoint.cs
@@ -2,6 +2,28 @@
 {
     public class ImmutablePoint
     {
+        public int X { get; }
+        public int Y { get; }
+
+        public ImmutablePoint(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public ImmutablePoint ChangeX(int newX)
+        {
+            return new ImmutablePoint(newX, Y);
+        }
         
+        public ImmutablePoint ChangeY(int newY)
+        {
+            return new ImmutablePoint(X, newY);
+        }
+
+        public override string ToString()
+        {
+            return "ImmutablePoint(" + X + ", " + Y + ")";
+        }
     }
 }
--- hw02/ImmutableType/Program.cs
+++ hw02/ImmutableType/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ImmutableType
+﻿using System;
+
+namespace ImmutableType
 {
     internal static class Program
     {
         public static void Main(string[] args)
         {
-            
+            var pointA = new ImmutablePoint(1, 2);
+            Console.WriteLine("pointA.X = {0}, pointA.Y = {1}", pointA.X, pointA.Y);
+
+            // pointA.X = 1; // not allowed
+            var pointB = pointA.ChangeX(5);
+            var pointC = pointB.ChangeY(7);
+            Console.WriteLine("pointB = " + pointB);
+            Console.WriteLine("pointC = " + pointC);
         }
     }
 }
