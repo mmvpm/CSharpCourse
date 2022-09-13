572f3c0842278a98a2e742382680ad32c38e321f
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-13 15:47:46 +0300

    add comments

--- hw02/ImmutableType/Program.cs
+++ hw02/ImmutableType/Program.cs
@@ -7,13 +7,14 @@ namespace ImmutableType
         public static void Main(string[] args)
         {
             var pointA = new ImmutablePoint(1, 2);
-            Console.WriteLine("pointA.X = {0}, pointA.Y = {1}", pointA.X, pointA.Y);
+            Console.WriteLine("pointA.X = {0}, pointA.Y = {1}", pointA.X, pointA.Y); // pointA.X = 1, pointA.Y = 2
 
             // pointA.X = 1; // not allowed
             var pointB = pointA.ChangeX(5);
             var pointC = pointB.ChangeY(7);
-            Console.WriteLine("pointB = " + pointB);
-            Console.WriteLine("pointC = " + pointC);
+            Console.WriteLine("pointA = " + pointA); // pointA = ImmutablePoint(1, 2)
+            Console.WriteLine("pointB = " + pointB); // pointB = ImmutablePoint(5, 2)
+            Console.WriteLine("pointC = " + pointC); // pointC = ImmutablePoint(5, 7)
         }
     }
 }
