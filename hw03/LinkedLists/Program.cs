a5cdd6d21de611215eeb9755ffdf65b305864c7c
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-20 15:02:26 +0300

    add LinkedLists

---
+++ hw03/LinkedLists/Program.cs
@@ -0,0 +1,36 @@
+﻿using hw03;
+
+T? FirstCommonNode<T>(LinkedList<T> first, LinkedList<T> second) where T : class
+{
+    var firstCurrentNode  = first.First;
+    var secondCurrentNode = second.First;
+    
+    while (firstCurrentNode != null && secondCurrentNode != null)
+    {
+        if (firstCurrentNode.ValueRef.Equals(secondCurrentNode.ValueRef))
+        {
+            return firstCurrentNode.Value;
+        }
+        firstCurrentNode = firstCurrentNode.Next;
+        secondCurrentNode = secondCurrentNode.Next;
+    }
+
+    return null;
+}
+
+var refWrapper = new RefWrapper(17); // common node
+var firstList  = new LinkedList<RefWrapper>(new[]
+{
+    new RefWrapper(0), new RefWrapper(1), refWrapper, new RefWrapper(2)
+});
+var secondList = new LinkedList<RefWrapper>(new[]
+{
+    new RefWrapper(0), new RefWrapper(2), refWrapper
+});
+var emptyList = new LinkedList<RefWrapper>();
+
+var firstCommonNode = FirstCommonNode(firstList, secondList);
+Console.WriteLine(firstCommonNode == null ? "null" : firstCommonNode); // Ref(17)
+
+var shouldBeNull = FirstCommonNode(firstList, emptyList);
+Console.WriteLine(shouldBeNull == null ? "null" : shouldBeNull); // null
---
+++ hw03/LinkedLists/RefWrapper.cs
@@ -0,0 +1,16 @@
+﻿namespace hw03;
+
+internal class RefWrapper
+{
+    private readonly int _value;
+
+    public RefWrapper(int value)
+    {
+        _value = value;
+    }
+
+    public override string ToString()
+    {
+        return "Ref(" + _value + ")";
+    }
+}
