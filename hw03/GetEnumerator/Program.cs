c3104ec7cfcaa1743980e2d5ac1990f0a2d2247f
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-20 16:40:59 +0300

    add GetEnumerator

---
+++ hw03/GetEnumerator/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace GetEnumerator;
+
+/*
+    var x = new
+    {
+        Items = new List<int> { 1, 2, 3 }.GetEnumerator()
+    };
+    while (x.Items.MoveNext())
+        Console.WriteLine(x.Items.Current);
+*/
+
+/*
+    > Если запустить, то будут бесконечно выводиться нули.
+
+    > Если посмотреть во что декомпилируется эта программа, а потом привести её к человеко-читаемому виду,
+    то получим примерно следующий код:
+*/
+
+internal sealed class AnonymousType<T>
+{
+    public readonly T Items;
+
+    public AnonymousType(T items)
+    {
+        Items = items;
+    }
+}
+
+internal static class Program
+{
+    private static void Main()
+    {
+        var list = new List<int> { 4, 4, 4 };
+        var anon = new AnonymousType<List<int>.Enumerator>(list.GetEnumerator());
+        while (anon.Items.MoveNext())
+        {
+            Console.WriteLine(anon.Items.Current);
+        }
+    }
+}
+
+/*
+    > В строке 35 получаем предупреждение от IDE
+    "Possibly impure struct method called on readonly variable: struct value always copied before invocation"
+    То есть при обращении к Items происходит копирование структуры List<int>.Enumerator
+
+    > В документации Microsoft сказано, что
+    "Initially, the enumerator is positioned before the first element in the collection.
+    At this position, Current is undefined"
+    Поэтому при обращении к Current мы получаем 0 -- значение по умолчанию для int
+*/
---
+++ hw03/PalindromeConstructor/Program.cs
@@ -0,0 +1,53 @@
+﻿bool isPalindrome(long number)
+{
+    var stringNumber = number.ToString();
+    for (var i = 0; i < stringNumber.Length / 2; i++)
+    {
+        if (stringNumber[i] != stringNumber[stringNumber.Length - i - 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+(long, int)? PalSeq(long palindrome)
+{
+    if (palindrome < 10)
+    {
+        return (palindrome, 0);
+    }
+
+    for (var startNumber = 10L; startNumber < 1e4; startNumber++)
+    {
+        var steps = 0;
+        var current = startNumber;
+        while (current < palindrome && !isPalindrome(current))
+        {
+            var reversed = string.Join("", current.ToString().Reverse());
+            current += long.Parse(reversed!);
+            steps += 1;
+        }
+
+        if (current == palindrome)
+        {
+            return (startNumber, steps);
+        }
+    }
+    return null;
+}
+
+var examples = new List<long> { 4884, 1, 0, 11, 3113, 8836886388 };
+foreach (var example in examples)
+{
+    Console.WriteLine("PalSeq(" + example + ") -> " + PalSeq(example));
+}
+/*
+    PalSeq(4884) -> (69, 4)
+    PalSeq(1) -> (1, 0)
+    PalSeq(0) -> (0, 0)
+    PalSeq(11) -> (10, 1)
+    PalSeq(3113) -> (199, 3)
+    PalSeq(8836886388) -> (177, 15)
+*/
