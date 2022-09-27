c237c4875923b29ff17957adde6f6e1103c9018d
IdeaSeeker <stroganov.n.36@gmail.com>
2022-09-27 16:12:14 +0300

    add Hamsters

---
+++ hw04/Hamsters/Hamster.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata;
+
+namespace Hamsters;
+
+public class Hamster : IComparable
+{
+    private readonly int _age;
+    private readonly double _weight;
+    private readonly double _height;
+    private readonly double _woolValue;
+
+    private Hamster(int age, double weight, double height, double woolValue)
+    {
+        _age = age;
+        _weight = weight;
+        _height = height;
+        _woolValue = woolValue;
+    }
+
+    public override string ToString()
+    {
+        return "Hamster(" + _age + ", " + _weight + ", " + _height + ", " + _woolValue + ")";
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj is not Hamster other)
+            return 1;
+
+        if (_age < other._age)
+            return -1;
+        if (_age > other._age)
+            return 1;
+
+        var thisSomeValue = _height + _weight;
+        var otherSomeValue = other._height + other._weight;
+        if (thisSomeValue < otherSomeValue)
+            return -1;
+        if (thisSomeValue > otherSomeValue)
+            return 1;
+
+        if (_woolValue < other._woolValue)
+            return -1;
+        return _woolValue > other._woolValue ? 1 : 0;
+    }
+
+    public static Hamster CreateRandom()
+    {
+        var random = new Random();
+        return new Hamster(
+            age: Math.Abs(random.Next()) % 365,
+            weight: Math.Round(random.NextDouble(), 3),
+            height: Math.Round(random.NextDouble(), 3),
+            woolValue: Math.Round(random.NextDouble(), 3)
+        );
+    }
+}
---
+++ hw04/Hamsters/Program.cs
@@ -0,0 +1,30 @@
+﻿using Hamsters;
+
+var hamsters = Enumerable
+    .Repeat(0, 5)
+    .Select(_ => Hamster.CreateRandom())
+    .ToList();
+
+Console.WriteLine("Before");
+hamsters.ForEach(hamster => Console.WriteLine("  - " + hamster));
+
+hamsters.Sort();
+
+Console.WriteLine("\nAfter");
+hamsters.ForEach(hamster => Console.WriteLine("  - " + hamster));
+
+/*
+Before
+  - Hamster(286, 0,686, 0,226, 0,775)
+  - Hamster(226, 0,514, 0,003, 0,744)
+  - Hamster(10, 0,341, 0,905, 0,525)
+  - Hamster(241, 0,209, 0,392, 0,924)
+  - Hamster(87, 0,708, 0,303, 0,794)
+
+After
+  - Hamster(10, 0,341, 0,905, 0,525)
+  - Hamster(87, 0,708, 0,303, 0,794)
+  - Hamster(226, 0,514, 0,003, 0,744)
+  - Hamster(241, 0,209, 0,392, 0,924)
+  - Hamster(286, 0,686, 0,226, 0,775)
+*/
