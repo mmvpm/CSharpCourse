45bb604830253a160242921659749c5023c2ae39
IdeaSeeker <stroganov.n.36@gmail.com>
2022-10-12 01:12:51 +0300

    add FrogAndLake

---
+++ hw06/FrogAndLake/Lake.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace FrogAndLake;
+
+public class Lake : IEnumerable<int>
+{
+    private List<int> _stones;
+
+    public Lake(List<int> stones)
+    {
+        _stones = stones;
+    }
+
+    public IEnumerator<int> GetEnumerator()
+    {
+        var i = 0;
+        for (; i < _stones.Count; i += 2)
+        {
+            yield return _stones[i];
+        }
+
+        i -= 2;
+        if (_stones.Count % 2 == 0)
+            i += 1;
+
+        for (; i >= 0; i -= 2)
+        {
+            yield return _stones[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ hw06/FrogAndLake/Program.cs
@@ -0,0 +1,15 @@
+﻿using FrogAndLake;
+
+var lake1 = new Lake(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 });
+foreach (var stone in lake1)
+{
+    Console.Write(stone + " ");
+}
+Console.WriteLine(); // 1 3 5 7 8 6 4 2
+
+var lake2 = new Lake(new List<int> { 13, 23, 1, -8, 4, 9 });
+foreach (var stone in lake2)
+{
+    Console.Write(stone + " ");
+}
+Console.WriteLine(); // 13 1 4 9 -8 23
