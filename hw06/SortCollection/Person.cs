981f020a8166147fb44eeabdb1a9fd735fc44b78
IdeaSeeker <stroganov.n.36@gmail.com>
2022-10-12 01:29:18 +0300

    add SortCollection

---
+++ hw06/SortCollection/Person.cs
@@ -0,0 +1,44 @@
+﻿namespace SortCollection;
+
+public class Person
+{
+    public string Name { get; set; }
+    
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return "Person(" + Name + ", " + Age + ")";
+    }
+}
+
+internal class ByNameComparer : IComparer<Person>
+{
+    public int Compare(Person? p1, Person? p2)
+    {
+        if (p1 is null || p2 is null)
+            throw new ArgumentException("Person should be not null");
+        if (p1.Name.Length < p2.Name.Length)
+            return -1;
+        if (p1.Name.Length > p2.Name.Length)
+            return +1;
+        return p1.Name.Length == 0 ? 
+            0 : p1.Name.ToLower()[0].CompareTo(p2.Name.ToLower()[0]);
+    }
+}
+
+internal class ByAgeComparer : IComparer<Person>
+{
+    public int Compare(Person? p1, Person? p2)
+    {
+        if (p1 is null || p2 is null)
+            throw new ArgumentException("Person should be not null");
+        return p1.Age.CompareTo(p2.Age);
+    }
+}
---
+++ hw06/SortCollection/Program.cs
@@ -0,0 +1,43 @@
+﻿using SortCollection;
+
+var persons = new List<Person>()
+{
+    new Person("name1", 1),
+    new Person("Egor", 27),
+    new Person("", 33),
+    new Person("b", 9),
+    new Person("aaa", 0),
+    new Person("name2", 2),
+    new Person("Masha", 21),
+    new Person("AAA", 70),
+    new Person("Misha", 18),
+};
+
+persons.Sort(new ByNameComparer());
+Console.WriteLine(string.Join("\n", persons));
+/* Output:
+    Person(, 33)
+    Person(b, 9)
+    Person(aaa, 0)
+    Person(AAA, 70)
+    Person(Egor, 27)
+    Person(Masha, 21)
+    Person(Misha, 18)
+    Person(name1, 1)
+    Person(name2, 2)
+*/
+Console.WriteLine();
+
+persons.Sort(new ByAgeComparer());
+Console.WriteLine(string.Join("\n", persons));
+/* Output:
+    Person(aaa, 0)
+    Person(name1, 1)
+    Person(name2, 2)
+    Person(b, 9)
+    Person(Misha, 18)
+    Person(Masha, 21)
+    Person(Egor, 27)
+    Person(, 33)
+    Person(AAA, 70)
+*/
