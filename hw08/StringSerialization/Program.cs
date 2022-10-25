acf15d73287be9d767c0cb004c430733d56872a6
IdeaSeeker <stroganov.n.36@gmail.com>
2022-10-25 14:33:24 +0300

    add StringSerialization

---
+++ hw08/StringSerialization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+string Identity(string s)
+{
+    var bytes = Encoding.UTF8.GetBytes(s);
+    var newS = Encoding.UTF8.GetString(bytes);
+    return newS;
+}
+
+const string russianString = "Строчка на русском языке";
+const string germanString = "Das ist nur eine Straße";
+const string japaneseString = "あなたは多くの価値があります。";
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine(Identity(russianString));
+Console.WriteLine(Identity(russianString) == russianString); // True
+
+Console.WriteLine(Identity(germanString));
+Console.WriteLine(Identity(germanString) == germanString); // True
+
+Console.WriteLine(Identity(japaneseString));
+Console.WriteLine(Identity(japaneseString) == japaneseString); // True
