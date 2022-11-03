82f24ea97c71e83bac787150e032dcf635986c9c
IdeaSeeker <stroganov.n.36@gmail.com>
2022-11-03 14:33:36 +0300

    add Numbers

---
+++ hw09/Numbers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+const long n = 100_000_000;
+const int numberLength = 8;
+
+using Stream s = new FileStream("numbers.txt", FileMode.Create);
+for (long i = 0; i < n; ++i)
+{
+    var number = (i * 47907813L + 29763927L) % n;
+    var line = number.ToString();
+    line = new string('0', numberLength - line.Length) + line + "\n";
+    s.Write(Encoding.ASCII.GetBytes(line));
+}
