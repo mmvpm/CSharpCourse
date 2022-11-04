49be0a9bc9abf8f94881c3c9a7e7c98daed38795
IdeaSeeker <stroganov.n.36@gmail.com>
2022-11-04 15:01:59 +0300

    add FileSearch

---
+++ hw09/FileSearch/FileSearch.cs
@@ -0,0 +1,32 @@
+﻿using System.Security;
+
+namespace FileSearch;
+
+public static class FileSearch
+{
+    public static FileInfo? FindFile(string fileName)
+    {
+        return FindFile(fileName, new DirectoryInfo(@"C:\"));
+    }
+
+    private static FileInfo? FindFile(string fileNameToFind, DirectoryInfo inDirectory)
+    {
+        try
+        {
+            var mbFileInfo = inDirectory.EnumerateFiles()
+                .FirstOrDefault(fileInfo => fileInfo.Name == fileNameToFind);
+            if (mbFileInfo != null)
+                return mbFileInfo;
+
+            return inDirectory.GetDirectories()
+                .Select(directory => FindFile(fileNameToFind, directory))
+                .FirstOrDefault(fileInfo => fileInfo != null);
+        }
+        catch (Exception ex)
+        {
+            if (ex is SecurityException || ex is DirectoryNotFoundException || ex is UnauthorizedAccessException)
+                return null;
+            throw;
+        }
+    }
+}
---
+++ hw09/FileSearch/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine(FileSearch.FileSearch.FindFile("c++.exe"));
+// C:\MinGW\mingw64\mingw64\bin\c++.exe
+
+Console.WriteLine(FileSearch.FileSearch.FindFile("kafka-console-consumer.bat"));
+// C:\Tools\Kafka\kafka_2.13-3.2.3\bin\windows\kafka-console-consumer.bat
