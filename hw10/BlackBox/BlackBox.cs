f8dfaeb453d84a41ad31d0ad516ec353d6497346
IdeaSeeker <stroganov.n.36@gmail.com>
2022-11-08 14:26:28 +0300

    add BlackBox

---
+++ hw10/BlackBox/BlackBox.cs
@@ -0,0 +1,36 @@
+﻿namespace BlackBox;
+
+public class BlackBox
+{
+    private int innerValue;
+
+    private BlackBox()
+    {
+        this.innerValue = 0;
+    }
+
+    private BlackBox(int innerValue)
+    {
+        this.innerValue = innerValue;
+    }
+
+    private void Add(int addend)
+    {
+        this.innerValue += addend;
+    }
+
+    private void Subtract(int subtrahend)
+    {
+        this.innerValue -= subtrahend;
+    }
+
+    private void Multiply(int multiplier)
+    {
+        this.innerValue *= multiplier;
+    }
+
+    private void Divide(int divider)
+    {
+        this.innerValue /= divider;
+    }
+}
---
+++ hw10/BlackBox/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+
+const BindingFlags bindingFlags = BindingFlags.NonPublic | BindingFlags.Instance;
+
+var blackBoxType = typeof(BlackBox.BlackBox);
+var blackBox = (BlackBox.BlackBox) blackBoxType
+    .GetConstructor(bindingFlags, null, new[] { typeof(int) }, null)!
+    .Invoke(new object[]{ 0 });
+
+while (true)
+{
+    var command = Console.ReadLine()!;
+    var parsedCommand = command.Split('(', ')');
+    var methodName = parsedCommand[0];
+    var argument = int.Parse(parsedCommand[1]);
+
+    var method = blackBoxType.GetMethod(methodName, bindingFlags);
+    if (method == null)
+    {
+        Console.WriteLine("No such method: {0}", methodName);
+        continue;
+    }
+    method.Invoke(blackBox, new object[]{ argument });
+
+    var innerValue = (int) blackBoxType
+        .GetField("innerValue", bindingFlags)!
+        .GetValue(blackBox)!;
+    Console.WriteLine(innerValue);
+}
+
+/* Output:
+ 
+> Add(100)
+> 100
+> Subtract(20)
+> 80
+> Divide(5)
+> 16
+> Multiply(2)
+> 32
+> Pow(3)
+> No such method: Pow
+*/
