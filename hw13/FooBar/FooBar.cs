6e6146cb27ba0f5a523ad4727fd28fc7f5896565
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-02 18:00:18 +0300

    add FooBar

---
+++ hw13/FooBar/FooBar.cs
@@ -0,0 +1,24 @@
+﻿namespace FooBar;
+
+public class FooBar {
+
+    private readonly int _n;
+
+    public FooBar(int n) {
+        _n = n;
+    }
+
+    public void Foo(Action printFoo) {
+        for (var i = 0; i < _n; i++) {
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+        }
+    }
+    
+    public void Bar(Action printBar) {
+        for (var i = 0; i < _n; i++) {
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+        }
+    }
+}
---
+++ hw13/FooBar/Program.cs
@@ -0,0 +1,68 @@
+﻿void PrintNTimes(int n)
+{
+    var counter = 0;
+    object locker = new();
+    var foobar = new FooBar.FooBar(n);
+    
+    void PrintFoo()
+    {
+        while (true)
+        {
+            lock (locker)
+            {
+                if (counter % 2 != 0) continue;
+                counter += 1;
+                Console.Write("foo");
+                break;
+            }
+        }
+    }
+
+    void PrintBar()
+    {
+        while (true)
+        {
+            lock (locker)
+            {
+                if (counter % 2 != 1) continue;
+                counter += 1;
+                Console.WriteLine("bar");
+                break;
+            }
+        }
+    }
+
+    var t1 = new Thread(_ => foobar.Foo(PrintFoo));
+    var t2 = new Thread(_ => foobar.Bar(PrintBar));
+
+    Console.WriteLine("n = " + n + ":");
+    t1.Start();
+    t2.Start();
+    t1.Join();
+    t2.Join();
+    Console.WriteLine();
+}
+
+PrintNTimes(1);
+PrintNTimes(3);
+PrintNTimes(7);
+
+/* Output:
+
+    n = 1:
+    foobar
+
+    n = 3:
+    foobar
+    foobar
+    foobar
+
+    n = 7:
+    foobar
+    foobar
+    foobar
+    foobar
+    foobar
+    foobar
+    foobar
+*/
