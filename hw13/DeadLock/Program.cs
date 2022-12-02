efe77832219acc78445e41494c5e6cf15cdd4ccf
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-02 16:14:34 +0300

    Add DeadLock

---
+++ hw13/DeadLock/Program.cs
@@ -0,0 +1,34 @@
+﻿object locker1 = new();
+object locker2 = new();
+
+void Function1()
+{
+    lock (locker1)
+    {
+        Thread.Sleep(1000);
+        lock (locker2)
+        {
+            Console.WriteLine("function 1");
+        }
+    }
+}
+
+void Function2()
+{
+    lock (locker2)
+    {
+        Thread.Sleep(1000);
+        lock (locker1)
+        {
+            Console.WriteLine("function 2");
+        }
+    }
+}
+
+var t1 = new Thread(Function1);
+var t2 = new Thread(Function2);
+
+t1.Start();
+t2.Start();
+
+// Output: <nothing>
