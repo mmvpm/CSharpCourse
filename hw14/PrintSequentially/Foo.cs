d6448ca4c7597156f9f0f7eb58be91091e35f096
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-06 21:39:11 +0300

    Add PrintSequentially

---
+++ hw14/PrintSequentially/Foo.cs
@@ -0,0 +1,19 @@
+﻿namespace PrintSequentially;
+
+public static class Foo
+{
+    public static void First()
+    {
+        Console.Write("first");
+    }
+
+    public static void Second()
+    {
+        Console.Write("second");
+    }
+
+    public static void Third()
+    {
+        Console.Write("third");
+    }
+}
---
+++ hw14/PrintSequentially/Program.cs
@@ -0,0 +1,53 @@
+﻿using PrintSequentially;
+
+int counter;
+object locker;
+
+void Print(int order, Action action)
+{
+    lock (locker)
+    {
+        while (counter < order)
+            Monitor.Wait(locker);
+        action();
+        counter += 1;
+        Monitor.PulseAll(locker);
+    }
+}
+
+void Example()
+{
+    counter = 0;
+    locker = new object();
+
+    var a = new Thread(() => Print(0, Foo.First));
+    var b = new Thread(() => Print(1, Foo.Second));
+    var c = new Thread(() => Print(2, Foo.Third));
+    var threads = new List<Thread> { a, b, c };
+
+    var random = new Random();
+    var threadsShuffled = threads.OrderBy(_ => random.Next()).ToList();
+    threadsShuffled.ForEach(t => t.Start());
+    threadsShuffled.ForEach(t => t.Join());
+
+    Console.WriteLine();
+}
+
+for (var i = 0; i < 10; ++i)
+{
+    Example();
+}
+
+/* Output:
+
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+    firstsecondthird
+*/
