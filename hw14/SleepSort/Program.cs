af973f40a2e92172ac6911811efd45489e64d687
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-06 21:50:28 +0300

    Add SleepSort

---
+++ hw14/SleepSort/Program.cs
@@ -0,0 +1,31 @@
+﻿const int maxListSize = 100;
+
+List<string> Sort(List<string> list)
+{
+    object locker = new();
+    var result = new List<string>();
+
+    var threads = list.Select(item => {
+        return new Thread(() => {
+            Thread.Sleep(item.Length * maxListSize);
+            lock (locker)
+            {
+                result.Add(item);
+            }
+        });
+    }).ToList();
+    
+    threads.ForEach(t => t.Start());
+    threads.ForEach(t => t.Join());
+
+    return result;
+}
+
+var list = new List<string>
+{
+    "scala", "c", "cpp", "java", "rust", "c#", "kotlin",
+    "f#", "haskell", "python", "go", "sql", "javascript"
+};
+var sortedList = Sort(list);
+Console.WriteLine(string.Join(" ", sortedList));
+// Output: c f# go c# cpp sql rust java scala kotlin python haskell javascript
