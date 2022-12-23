0510725f49e530ba4c4da03a87527b34395751cb
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-23 12:35:10 +0300

    Add ZeroEvenOdd

---
+++ hw16/ZeroEvenOddSequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using ZeroEvenOddSequence;
+
+void Simulate(int n)
+{
+    var zeroEvenOdd = new ZeroEvenOdd(n);
+    var threads = new List<Thread>
+    {
+        new(() => zeroEvenOdd.Zero(Console.Write)),
+        new(() => zeroEvenOdd.Even(Console.Write)),
+        new(() => zeroEvenOdd.Odd(Console.Write))
+    };
+    threads.ForEach(t => t.Start());
+    threads.ForEach(t => t.Join());
+    Console.WriteLine();
+}
+
+Simulate(1); // 01
+Simulate(2); // 0102
+Simulate(5); // 0102030405
+Simulate(9); // 010203040506070809
---
+++ hw16/ZeroEvenOddSequence/ZeroEvenOdd.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+
+namespace ZeroEvenOddSequence;
+
+public class ZeroEvenOdd
+{
+    private readonly int _n;
+
+    private int _order = 1;
+    private readonly object _orderLock = new();
+
+    public ZeroEvenOdd(int n)
+    {
+        _n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    public void Zero(Action<int> printNumber) =>
+        CommonPart(cycleSize: 2, cycleNum: 1, order => printNumber(0));
+
+    public void Even(Action<int> printNumber) =>
+        CommonPart(cycleSize: 4, cycleNum: 0, order => printNumber(order / 2));
+
+    public void Odd(Action<int> printNumber) =>
+        CommonPart(cycleSize: 4, cycleNum: 2, order => printNumber(order / 2));
+
+    private void CommonPart(int cycleSize, int cycleNum, Action<int> printNumberByOrder)
+    {
+        while (_order <= 2 * _n)
+        {
+            lock (_orderLock)
+            {
+                while (_order % cycleSize != cycleNum)
+                {
+                    Monitor.Wait(_orderLock);
+                }
+
+                if (_order <= 2 * _n)
+                {
+                    printNumberByOrder(_order);
+                }
+                _order += 1;
+                Monitor.PulseAll(_orderLock);
+            }
+        }
+    }
+}
