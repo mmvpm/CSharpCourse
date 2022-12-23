f9ce55406dfc7cd7d8ac53a3342a6e2b418f82c0
IdeaSeeker <stroganov.n.36@gmail.com>
2022-12-23 13:22:11 +0300

    Add ParallelArray

---
+++ hw16/ParallelArrayOperations/ParallelArray.cs
@@ -0,0 +1,71 @@
+﻿namespace ParallelArrayOperations;
+
+public class ParallelArray
+{
+    private readonly List<int> _array;
+    private readonly Random _random;
+    private bool _terminated;
+    private readonly ReaderWriterLockSlim _readWriteLock;
+    
+    public ParallelArray(List<int> array)
+    {
+        _array = array;
+        _random = new Random();
+        _terminated = false;
+        _readWriteLock = new ReaderWriterLockSlim();
+    }
+    
+    public void ThreadAverage()
+    {
+        while (!_terminated)
+        {
+            _readWriteLock.EnterReadLock();
+            var result = _array.Average();
+            Console.WriteLine("Average: " + result);
+            _readWriteLock.ExitReadLock();
+            Thread.Sleep(_random.Next(10, 100));
+        }
+    }
+    
+    public void ThreadMin()
+    {
+        while (!_terminated)
+        {
+            _readWriteLock.EnterReadLock();
+            var result = _array.Min();
+            Console.WriteLine("Min: " + result);
+            _readWriteLock.ExitReadLock();
+            Thread.Sleep(_random.Next(10, 100));
+        }
+    }
+    
+    public void ThreadSwap()
+    {
+        while (!_terminated)
+        {
+            _readWriteLock.EnterWriteLock();
+            var (i, j) = (_random.Next(_array.Count), _random.Next(_array.Count));
+            (_array[i], _array[j]) = (_array[j], _array[i]);
+            Console.WriteLine("Swap: array[" + i + "] = " + _array[i] + " & array[" + j + "] = " + _array[j]);
+            _readWriteLock.ExitWriteLock();
+            Thread.Sleep(_random.Next(50, 200));
+        }
+    }
+    
+    public void ThreadSort()
+    {
+        while (!_terminated)
+        {
+            _readWriteLock.EnterWriteLock();
+            _array.Sort();
+            Console.WriteLine("Sort: array = [" + string.Join(", ", _array) + "]");
+            _readWriteLock.ExitWriteLock();
+            Thread.Sleep(_random.Next(100, 1000));
+        }
+    }
+
+    public void Terminate()
+    {
+        _terminated = true;
+    } 
+}
---
+++ hw16/ParallelArrayOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using ParallelArrayOperations;
+
+var array = new List<int> { 1, 5, 2, 3, 3, 1, 7, 5 };
+var parallelArray = new ParallelArray(array);
+
+var threads = new List<Thread>
+{
+    new(() => parallelArray.ThreadAverage()),
+    new(() => parallelArray.ThreadMin()),
+    new(() => parallelArray.ThreadMin()),
+    new(() => parallelArray.ThreadSwap()),
+    new(() => parallelArray.ThreadSwap()),
+    new(() => parallelArray.ThreadSort()),
+};
+threads.ForEach(t => t.Start());
+
+Thread.Sleep(300);
+parallelArray.Terminate();
+threads.ForEach(t => t.Join());
+
+/* Output:
+
+    Min: 1
+    Min: 1
+    Swap: array[6] = 3 & array[3] = 7
+    Sort: array = [1, 1, 2, 3, 3, 5, 5, 7]
+    Swap: array[4] = 7 & array[7] = 3
+    Average: 3,375
+    Min: 1
+    Average: 3,375
+    Average: 3,375
+    Min: 1
+    Min: 1
+    Swap: array[7] = 3 & array[7] = 3
+    Min: 1
+    Average: 3,375
+    Min: 1
+    Average: 3,375
+    Swap: array[0] = 1 & array[0] = 1
+    Min: 1
+    Min: 1
+    Swap: array[2] = 2 & array[2] = 2
+    Average: 3,375
+    Min: 1
+*/
